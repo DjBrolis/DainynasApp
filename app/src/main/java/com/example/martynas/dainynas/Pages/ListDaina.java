ace11e31635c09569bfb75b1f2fd38e6fa82864c
DjBrolis <martynasgrigas@hotmail.com>
2016-09-07 00:01:45 +0300

    pasalintas search

--- app/src/main/java/com/example/martynas/dainynas/Pages/ListDaina.java
+++ app/src/main/java/com/example/martynas/dainynas/Pages/ListDaina.java
@@ -55,29 +55,8 @@ public class ListDaina extends AppCompatActivity {
 
         populateListView ();
         registerDainosPasirinkimas();
-        handleIntent(getIntent());
-        
+
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.options_menu, menu);
-
-        // Associate searchable configuration with the SearchView
-        SearchManager searchManager =
-                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView =
-                (SearchView) menu.findItem(R.id.search).getActionView();
-        searchView.setSearchableInfo(
-                searchManager.getSearchableInfo(getComponentName()));
-
-
-
-
-        return true;
-    }
-
     public void populateListView() {
 
         List<Daina> dainuSarasas = new Select().from(Daina.class).orderBy("Pavadinimas ASC").execute();
